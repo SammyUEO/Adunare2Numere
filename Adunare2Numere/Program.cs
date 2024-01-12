faf87814d58b0e4fe8bd29209306c3d80332551e
SammyUEO <samuel.pop@emanuel.ro>
2024-01-12 10:09:44 +0200

    tema

---
+++ Adunare2Numere/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine();
+        int numar1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+        int numar2 = Convert.ToInt32(Console.ReadLine());
+
+        int suma = numar1 + numar2;
+
+        Console.WriteLine( suma);
+    }
+}
